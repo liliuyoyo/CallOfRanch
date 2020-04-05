d5ca8c0cf0f0f0c422110501730b1c66a8454ad7
liliuyoyo <liliuyoyo@gmail.com>
2020-04-05 13:26:07 -0500

    Added cropping trees functionality

---
+++ Assets/Scripts/Cropping.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cropping : MonoBehaviour
+{
+    //Variables
+    public GameObject axe;
+    private bool isEquiped = false;
+    private int rayCastLength = 10;
+
+    private void Update()
+    {
+        if(!axe.activeSelf && Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            isEquiped = true;
+            axe.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            isEquiped = false;
+            axe.SetActive(false);
+        }
+
+        //Raycast
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayCastLength))
+        {
+            if(hit.collider.tag == "tree" && Input.GetButtonDown("Crop") && isEquiped == true)
+            {
+                TreeScript treeScript = hit.collider.gameObject.GetComponent<TreeScript>();
+                treeScript.treeHealth--;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    //Variables
+    public Transform logs;
+    GameObject thisTree;
+    Rigidbody rb;
+    public int treeHealth = 5;
+
+    private void Start()
+    {
+        thisTree = this.gameObject;
+        rb = thisTree.AddComponent<Rigidbody>();
+        rb.isKinematic = true;
+        rb.useGravity = true;
+        rb.mass = 10;
+    }
+
+    private void Update()
+    {
+        if(treeHealth <= 0)
+        {
+            rb.isKinematic = false;
+            rb.AddForce(Vector3.forward, ForceMode.Impulse);
+            StartCoroutine(destroyTree());
+        }
+    }
+
+    private IEnumerator destroyTree()
+    {
+        yield return new WaitForSeconds(4);
+        Destroy(thisTree);
+
+        Vector3 position = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+        Instantiate(logs, thisTree.transform.position + new Vector3(0, 0, 0) + position, Quaternion.Euler(270f, 0f, 0f));
+        Instantiate(logs, thisTree.transform.position + new Vector3(2, 2, 0) + position, Quaternion.Euler(270f, 10f, 0f));
+        Instantiate(logs, thisTree.transform.position + new Vector3(5, 5, 0) + position, Quaternion.Euler(270f, 70f, 0f));
+
+    }
+}
