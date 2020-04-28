775bd9d0931a9baa36b70073dc4b4c3a4562b46a
Smileincode <31875661+Smileincode@users.noreply.github.com>
2020-04-28 16:52:44 -0500

    Fix ignite bug

---
+++ Assets/Scripts/ControlIgnite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlIgnite : MonoBehaviour
+{
+    public Transform sparkle;
+    //public Transform sparkle2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+        //sparkle2.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //  void OnCollisionEnter(Collision col)
+    // {
+    //     if (col.gameObject.name == "Torch")
+    //     {
+    //         sparkle1.GetComponent<ParticleSystem>().enableEmission = true;
+    //     }
+    // }
+
+    void OnTriggerEnter() 
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = true;
+        //sparkle2.GetComponent<ParticleSystem>().enableEmission = true;
+    }
+}
